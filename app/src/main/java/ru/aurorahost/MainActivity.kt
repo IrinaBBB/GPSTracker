497b565c400ecbb2d7bcb176466e9d671a9f7dae
IrinaB <balkarovairina@gmail.com>
2024-02-12 18:58:30 +0100

    Add ViewBinding

--- app/src/main/java/ru/aurorahost/MainActivity.kt
+++ app/src/main/java/ru/aurorahost/MainActivity.kt
@@ -2,10 +2,14 @@ package ru.aurorahost
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.aurorahost.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
