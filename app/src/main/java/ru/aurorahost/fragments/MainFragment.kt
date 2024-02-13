4746d14a1f586ea5eb5e42906b3de3cde1ad3b5e
IrinaB <balkarovairina@gmail.com>
2024-02-13 22:22:44 +0100

    Add Tracking Current Location

--- app/src/main/java/ru/aurorahost/fragments/MainFragment.kt
+++ app/src/main/java/ru/aurorahost/fragments/MainFragment.kt
@@ -9,6 +9,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import org.osmdroid.config.Configuration
 import org.osmdroid.library.BuildConfig
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.CustomZoomButtonsController
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 import ru.aurorahost.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
@@ -24,12 +28,36 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initMap()
+    }
+
     private fun setMap() {
         Configuration.getInstance().load(
             activity as AppCompatActivity,
             activity?.getSharedPreferences("osm_pref", Context.MODE_PRIVATE)
         )
         Configuration.getInstance().userAgentValue = BuildConfig.LIBRARY_PACKAGE_NAME
+    }
+
+    private fun initMap() = with(binding) {
+        map.apply {
+            setMultiTouchControls(true)
+            zoomController.setVisibility(CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT)
+        }
+
+        map.controller.apply {
+            setZoom(16.0)
+            val locationProvider = GpsMyLocationProvider(activity)
+            val locationOverlay = MyLocationNewOverlay(locationProvider, map)
+            locationOverlay.enableMyLocation()
+            locationOverlay.enableFollowLocation()
+            locationOverlay.runOnFirstFix {
+                map.overlays.clear()
+                map.overlays.add(locationOverlay)
+            }
+        }
     }
 
     companion object {
